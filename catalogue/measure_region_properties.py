477e104d192c750860d8a2c6639c1b7e8fd2d109
Piper Fowler-Wright <piperfw@gmail.com>
2026-08-13 16:12:26 +0100

    explictly include options and parse argument list in case malformed LLM json

--- catalogue/measure_region_properties.py
+++ catalogue/measure_region_properties.py
@@ -19,24 +19,43 @@ METADATA = {
         "extra_properties": {
             "type": "array",
             "description": "List of any additional properties to include. "
-            "Must be a property from skimage.measure.regionprops"
+            "Must be a property from skimage.measure.regionprops",
+            "options": [
+                'area', 'area_bbox', 'area_convex', 'area_filled',
+                'axis_major_length', 'axis_minor_length', 'bbox',
+                'centroid', 'centroid_local', 'centroid_weighted',
+                'centroid_weighted_local', 'coords', 'coords_scaled',
+                'eccentricity', 'equivalent_diameter_area', 'euler_number',
+                'extent', 'feret_diameter_max', 'image', 'image_convex',
+                'image_filled', 'image_intensity', 'inertia_tensor',
+                'inertia_tensor_eigvals', 'intensity_max', 'intensity_mean',
+                'intensity_median', 'intensity_min', 'intensity_std',
+                'moments', 'moments_central', 'moments_hu',
+                'moments_normalized', 'moments_weighted',
+                'moments_weighted_central', 'moments_weighted_hu',
+                'moments_weighted_normalized', 'num_pixels', 'orientation',
+                'perimeter', 'perimeter_crofton', 'solidity',
+            ],
         },
     },
     "required": ["image_data"],
     "tags": ["measure", "labels", "feature extraction", "shape", "intensity", "size"],
-    "dependencies": ["scikit-image","scipy","numpy"],
+    "dependencies": ["scikit-image", "scipy", "numpy"],
 }
 
 
-def measure_region_properties(image_data, extra_properties = []):
+def measure_region_properties(image_data, extra_properties=[]):
     import skimage as ski
-
-    defaults = ["label","perimeter","area","centroid","num_pixels"]
-    all_props = set.union(set(defaults),set(extra_properties))
-
-    props = ski.measure.regionprops_table(label_image = image_data['current'],
-                                          intensity_image = image_data['source'],
-                                          properties = tuple(all_props))  
-
+    defaults = ["label", "perimeter", "area", "centroid", "num_pixels"]
+    try:
+        _extra = list(extra_properties)
+    except TypeError:
+        _extra = []
+    all_props = set.union(set(defaults), set(_extra))
+    props = ski.measure.regionprops_table(
+        label_image=image_data['current'],
+        intensity_image=image_data['source'],
+        properties=tuple(all_props),
+    )
     image_data['info']['measurements'] = props
     return image_data
